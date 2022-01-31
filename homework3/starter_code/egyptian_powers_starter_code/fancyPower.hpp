083a9dfcca196c2a47df2f4c5472b2efe348fbf0
parallels <jacobrkerstetter@gmail.com>
2022-01-31 12:41:39 -0500

    worked on egyptian powers

--- homework3/starter_code/egyptian_powers_starter_code/fancyPower.hpp
+++ homework3/starter_code/egyptian_powers_starter_code/fancyPower.hpp
@@ -3,8 +3,16 @@
 
 int fancyPower(int n, int m)
 {
-	// your code should go here.
-	return 0;
+	if (n == 0)
+		return 1;
+	else if (n == 1)
+		return m;
+	else if (n == 2)
+		return m * m;
+	else if (n % 2 == 0)
+		return fancyPower(n / 2, fancyPower(2, m));
+	else
+		return m * fancyPower((n - 1) / 2, fancyPower(2, m));
 }
 
 #endif
--- homework3/starter_code/egyptian_powers_starter_code/fancyPower_tests.cpp
+++ homework3/starter_code/egyptian_powers_starter_code/fancyPower_tests.cpp
@@ -4,8 +4,42 @@
 #include "fancyPower.hpp"
 
 // write your test cases here
-TEST_CASE("Test case", "fancyPower"){
+TEST_CASE("Basic Test", "fancyPower"){
 
-  REQUIRE(true);
-  
+  REQUIRE(fancyPower(2,2) == 4);
+}
+
+TEST_CASE("0 Power", "fancyPower"){
+
+  REQUIRE(fancyPower(0,2) == 1);
+}
+
+TEST_CASE("1 Power", "fancyPower"){
+
+  REQUIRE(fancyPower(1,5) == 5);
+}
+
+TEST_CASE("2 Power", "fancyPower"){
+
+  REQUIRE(fancyPower(2,5) == 25);
+}
+
+TEST_CASE("Odd Power 1", "fancyPower"){
+
+  REQUIRE(fancyPower(7,2) == 128);
+}
+
+TEST_CASE("Odd Power 2", "fancyPower"){
+
+  REQUIRE(fancyPower(3,5) == 125);
+}
+
+TEST_CASE("Even Power 1", "fancyPower"){
+
+  REQUIRE(fancyPower(4,3) == 81);
+}
+
+TEST_CASE("Even Power 2", "fancyPower"){
+
+  REQUIRE(fancyPower(4,1) == 1);
 }
